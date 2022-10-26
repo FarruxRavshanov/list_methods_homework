f39934512f5f233b91bd9a2514a97233003ed183
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-26 19:40:26 +0300

    list1

--- List01.py
+++ List01.py
@@ -7,4 +7,7 @@ def main(fruits,x):
     Returns:
         list: return answer
     """
-    return fruits.append(x)
+    a = fruits.append(x)
+    return fruits
+
+print(main(['lemon', 'apple'], 'orange'))
